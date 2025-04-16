f7e60171d3792c97760fdc4e50e12ec3ec35c534
0122-0 <flowercats95@gmail.com>
2025-04-16 20:23:58 +0900

    레벨 1-3 : 코드 개선 리뷰

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +14,9 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
+
     @NotBlank
+    @Size(min = 8)
+    @Pattern(regexp="(?=.*[A-Z]*)(?=.*\\d*)", message = " 새 비밀번호에는 대문자와 숫자가 포함 되어 있어야 한다.")
     private String newPassword;
 }
